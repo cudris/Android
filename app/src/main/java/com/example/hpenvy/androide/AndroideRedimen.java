0af52ed0a99d75d362be6126d1148f08fd7a2396
cudris <cudris73@hotmail.com>
2017-02-15 23:15:44 -0500

    Initial commit

---
+++ app/src/main/java/com/example/hpenvy/androide/AndroideRedimen.java
@@ -0,0 +1,14 @@
+package com.example.hpenvy.androide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AndroideRedimen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_androide_redimen);
+
+    }
+}
